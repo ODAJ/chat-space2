03de33c277e6cdc757817ed145add46ab9aa2b73
oda <chunyex@gmail.com>
2019-08-16 12:20:01 +0900

    update.message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -60,14 +60,14 @@ var reloadMessages = function () {
       data: {id: last_message_id} 
     })
     .done(function (messages) { 
+      
       var insertHTML = '';
       messages.forEach(function (message) {
-        
         insertHTML = buildHTML(message);
         $('.messages').append(insertHTML);        
-      })
         $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight});
     })
+  })
     .fail(function () {
       alert('自動更新に失敗しました');
     });
